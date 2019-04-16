c542d9db7195484b92139b77fc48c76f3f8adac1
Jan Hořava <czechinthemail@gmail.com>
2019-04-16 13:33:44 -0700

    added mouse-lock setting

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ function draw_logic(){
 }
 
 function logic(){
+    if(core_storage_data['mouse-lock']
+      || core_mouse['down-0']){
+        core_entities[top_layer]['x'] = core_mouse['x'] - core_storage_data['layer-width'] / 2;
+        core_entities[top_layer]['y'] = core_mouse['y'] - core_storage_data['layer-height'] / 2;
+    }
+
     logic_recursive(last_entity);
 }
 
@@ -24,25 +30,23 @@ function repo_init(){
       },
       'info': '<input id=explode type=button value=Explode>',
       'mousebinds': {
-        'mousemove': {
-          'todo': function(){
-              core_entities[top_layer]['x'] = core_mouse['x'] - core_storage_data['layer-width'] / 2;
-              core_entities[top_layer]['y'] = core_mouse['y'] - core_storage_data['layer-height'] / 2;
-          },
-        },
+        'mousedown': {},
+        'mousemove': {},
       },
       'storage': {
         'layer-height': 100,
         'layer-random': 0,
         'layer-speed': 3,
         'layer-width': 100,
+        'mouse-lock': true,
         'snake-length': 99,
       },
       'storage-menu': '<table><tr><td><input id=layer-height><td>Layer Height'
         + '<tr><td><input id=layer-random><td>Layer Movement Randomness'
         + '<tr><td><input id=layer-width><td>Layer Width'
         + '<tr><td><input id=layer-speed><td>Layer Speed'
-        + '<tr><td><input id=snake-length><td>Length</table>',
+        + '<tr><td><input id=snake-length><td>Length'
+        + '<tr><td><input id=mouse-lock type=checkbox><td>Mouse Lock</table>',
       'title': 'LayeredSnake-2D3D.htm',
     });
     canvas_init();
