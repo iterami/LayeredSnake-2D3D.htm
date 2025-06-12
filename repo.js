cd2ed6098f3c4e742767050b00979014d9c72721
Jan Hořava <czechinthemail@gmail.com>
2025-06-12 11:43:47 -0700

    updated core_random_integer() usage

--- repo.js
+++ repo.js
@@ -75,12 +75,8 @@ function load_data(){
           'properties': {
             'color': '#' + core_random_hex(),
             'parent': parent_id,
-            'x': core_random_integer({
-              'max': canvas_properties['width'],
-            }) - core_storage_data['layer-width'] / 2,
-            'y': core_random_integer({
-              'max': canvas_properties['height'],
-            }) - core_storage_data['layer-height'] / 2,
+            'x': core_random_integer(canvas_properties['width']) - core_storage_data['layer-width'] / 2,
+            'y': core_random_integer(canvas_properties['height']) - core_storage_data['layer-height'] / 2,
           },
         });
 
